92a00c3727c8f3fe5faa002e8ffc4306d1338488
Ivan Volodartsev <untrupus@gmail.com>
2020-11-21 17:46:16 +0600

    add fixtures

--- app/products.js
+++ app/products.js
@@ -5,7 +5,11 @@ const Product = require('../models/Product');
 const auth = require('../middleware/auth');
 
 router.get('/', async (req, res) => {
-    const result = await Product.find();
+    let query;
+    if (req.query.category) {
+        query = {category: req.query.category}
+    }
+    const result = await Product.find(query);
     if (result) {
         res.send(result);
     } else {
@@ -31,10 +35,23 @@ router.post('/', auth, config.upload.single("image"), async (req, res) => {
     productData.user = req.user._id;
     const product = new Product(productData)
     try {
-            await product.save();
-            res.send(product);
+        await product.save();
+        res.send(product);
     } catch (e) {
         res.status(400).send(e);
+    }
+});
+
+router.delete('/:id', auth, async (req, res) => {
+    const firstResult = await Product.findById(req.params.id).populate({path: "user"});
+    if (firstResult.user.equal(req.user._id)) {
+
+        const result = await Product.findByIdAndDelete({_id: req.params.id});
+        if (result) {
+            res.send("Task removed");
+        } else {
+            res.sendStatus(403);
+        }
     }
 });
 
--- fixtures.js
+++ fixtures.js
@@ -2,10 +2,7 @@ const mongoose = require("mongoose");
 const {nanoid} = require("nanoid");
 const config = require("./config");
 const User = require("./models/User");
-const Artist = require("./models/Artist");
-const Album = require("./models/Album");
-const Track = require("./models/Track");
-const TrackHistory = require("./models/TrackHistory");
+const Product = require("./models/Product");
 
 mongoose.connect(config.db.url + "/" + config.db.name, {useNewUrlParser: true});
 
@@ -13,123 +10,77 @@ const db = mongoose.connection;
 
 db.once("open", async () => {
     try {
-        await db.dropCollection("albums");
-        await db.dropCollection("artists");
         await db.dropCollection("users");
-        await db.dropCollection("tracks");
-        await db.dropCollection("trackhistories");
+        await db.dropCollection("products");
+
     } catch (e) {
         console.log("Collection were not presented!");
     }
 
-    const [death, nin, morfer] = await Artist.create({
-        name: "Death",
-        image: "death.jpg"
-    }, {
-        name: "NIN",
-        image: "nin.png"
-    },{
-        name: "Morfer",
-        image: "morfer.png"
-    });
-
-    const [human, symbolic, spiral, tribunal] = await Album.create({
-        name: "Human",
-        year: 1991,
-        artist: death._id,
-        image: "Human.jpg"
-    }, {
-        name: "Symbolic",
-        year: 1995,
-        artist: death._id,
-        image: "symbolic.jpg"
-    }, {
-        name: "The Downward Spiral",
-        year: 1994,
-        artist: nin._id,
-        image: "downward.jpg"
-    }, {
-        name: "Tribunal",
-        year: 2020,
-        artist: morfer._id,
-        image: "tribunal.png"
-    });
-
-    const [lack, see, sacred, crystal, hurt, vandallen] = await Track.create({
-            name: "Lack Of Comprehension",
-            duration: "3.47",
-            album: human._id,
-            number: 5,
-            youtube: "7j8vUbMmOwM"
-        }, {
-            name: "See Through Dreams",
-            duration: "3.56",
-            album: human._id,
-            number: 6,
-        }, {
-            name: "Sacred Serenity",
-            duration: "4.29",
-            album: symbolic._id,
-            number: 3,
-            youtube: "-as4LausEok"
-        }, {
-            name: "Crystal mountain",
-            duration: "5.07",
-            album: symbolic._id,
-            number: 6,
-            youtube: "zguCFjHyVeM"
-        }, {
-            name: "Hurt",
-            duration: "3.56",
-            album: spiral._id,
-            number: 6,
-            youtube: "6oGqIfnIAEA"
-        }, {
-            name: "Vandallen",
-            duration: "6.29",
-            album: tribunal._id,
-            number: 666,
-            youtube: "4A2N2u9ee1o"
-        },
-    );
-
-
     const [user, user1] = await User.create({
         username: "user",
         password: "123",
+        displayName: "User",
+        phone: 772234345456,
         token: nanoid()
     }, {
         username: "user1",
         password: "123",
+        displayName: "User1",
+        phone: 772234345456,
         token: nanoid()
     });
 
-    await TrackHistory.create({
+    await Product.create({
+            title: "Product 1",
+            category: "cars",
             user: user._id,
-            track: lack._id,
-            datetime: "Wed Nov 18 2020 01:59:36 GMT+0600 (Kyrgyzstan Time)"
+            price: 225,
+            description: "product description",
+            image: "car.jpg"
         }, {
+            title: "Product 1",
+            category: "guns",
             user: user._id,
-            track: see._id,
-            datetime: "Wed Nov 18 2020 02:59:36 GMT+0600 (Kyrgyzstan Time)"
-        }, {
+            price: 567,
+            description: "product description",
+            image: "gun.jpg"
+        },{
+            title: "Product 1",
+            category: "clothes",
+            user: user1._id,
+            price: 123,
+            description: "product description",
+            image: "cloth.jpg"
+        },{
+            title: "Product 1",
+            category: "cars",
+            user: user1._id,
+            price: 225,
+            description: "product description",
+            image: "car.jpg"
+        },{
+            title: "Product 1",
+            category: "guns",
             user: user._id,
-            track: sacred._id,
-            datetime: "Wed Nov 18 2020 03:59:36 GMT+0600 (Kyrgyzstan Time)"
-        }, {
+            price: 8900,
+            description: "product description",
+            image: "gun.jpg"
+        },{
+            title: "Product 1",
+            category: "cars",
+            user: user._id,
+            price: 345,
+            description: "product description",
+            image: "car.jpg"
+        },{
+            title: "Product 1",
+            category: "computers",
             user: user1._id,
-            track: crystal._id,
-            datetime: "Wed Nov 18 2020 04:59:36 GMT+0600 (Kyrgyzstan Time)"
-        }, {
-            user: user1._id,
-            track: hurt._id,
-            datetime: "Wed Nov 18 2020 05:59:36 GMT+0600 (Kyrgyzstan Time)"
-        }, {
-            user: user1._id,
-            track: vandallen._id,
-            datetime: "Wed Nov 18 2020 05:19:36 GMT+0600 (Kyrgyzstan Time)"
+            price: 225,
+            description: "product description",
+            image: "comp.jpg"
         },
     );
-
     db.close();
 });
